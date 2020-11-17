b3d02f20b221bcf8647fdc6d43936ff05e3b6291
JanzenLiu <chengtsung_liu@yahoo.com>
2020-11-16 20:28:35 -0800

    finish online/util

---
+++ online/util/config.py
@@ -0,0 +1,4 @@
+class Config(object):
+    DATA_SOURCE_REDIS = "redis"
+    DATA_SOURCE_FILE = "file"
+    EMB_DATA_SOURCE = "file"  # cannot use Config.DATA_SOURCE_FILE here
---
+++ online/util/utility.py
@@ -0,0 +1,11 @@
+from ..model.embedding import Embedding
+
+
+class Utility(object):
+    @staticmethod
+    def parse_emb_str(embs_str: str):
+        emb_strs = embs_str.split()
+        emb = Embedding()
+        for emb_str in emb_strs:
+            emb.add_dim(float(emb_str))
+        return emb
